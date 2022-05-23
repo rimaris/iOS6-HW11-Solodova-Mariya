00911393e95b5515649c08b2da0525af94931461
maryvasileva <marysolodova1998@gmail.com>
2022-05-23 21:08:51 +0300

    Initial Commit

---
+++ iOS6-HW11-Solodova-Mariya/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS6-HW11-Solodova-Mariya
+//
+//  Created by Мария Солодова on 23.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
